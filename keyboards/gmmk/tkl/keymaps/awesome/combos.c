dd43da2e3181fcb050bb867f8d7320143ddedfc5
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-01-26 15:10:51 -0600

    enable combos for a more reliable alternative to hrm

---
+++ keyboards/gmmk/tkl/keymaps/awesome/combos.c
@@ -0,0 +1,102 @@
+enum combos {
+    L_1_2H_CTRL,
+    R_1_2H_CTRL,
+    // L_1_2U_SHIFT,
+    // R_1_2U_SHIFT,
+    // L_1_3H_ALT,
+    // R_1_3H_ALT,
+    // L_1_3U_WIN,
+    // R_1_3U_WIN,
+    // L_2H_3H_CTRL_SHIFT,
+    // R_2H_3H_CTRL_SHIFT,
+    // L_2H_3U_CTRL_ALT,
+    // R_2H_3U_CTRL_ALT,
+    // L_2U_3H_CTRL_WIN,
+    // R_2U_3H_CTRL_WIN,
+    // L_2U_3U_SHIFT_ALT,
+    // R_2U_3U_SHIFT_ALT,
+    // L_1_2H_3H_SHIFT_WIN,
+    // R_1_2H_3H_SHIFT_WIN,
+    // L_1_2H_3U_ALT_WIN,
+    // R_1_2H_3U_ALT_WIN,
+    // L_Z_CTRL_SHIFT_ALT,
+    // R_Z_CTRL_SHIFT_ALT,
+    // L_1_2U_3H_CTRL_SHIFT_WIN,
+    // R_1_2U_3H_CTRL_SHIFT_WIN,
+    // L_1_2U_3U_CTRL_ALT_WIN,
+    // R_1_2U_3U_CTRL_ALT_WIN,
+    // L_1U_2U_3U_SHIFT_ALT_WIN,
+    // R_1U_2U_3U_SHIFT_ALT_WIN,
+    // L_Z_O_CTRL_SHIFT_ALT_WIN,
+    // R_Z_O_CTRL_SHIFT_ALT_WIN,
+    COMBO_LENGTH // nifty trick to avoid manually specifying how many combos you have
+};
+
+uint16_t COMBO_LEN = COMBO_LENGTH; // nifty trick continued
+
+// define keys that make up combos
+const uint16_t PROGMEM l_1_2h_ctrl[] = {KC_F, KC_D, COMBO_END};
+const uint16_t PROGMEM r_1_2h_ctrl[] = {KC_J, KC_K, COMBO_END};
+// const uint16_t PROGMEM l_1_2u_shift[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_1_2u_shift[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM l_1_3h_alt[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_1_3h_alt[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM l_1_3u_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_1_3u_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM l_2h_3h_ctrl_shift[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_2h_3h_ctrl_shift[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM l_2h_3u_ctrl_alt[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_2h_3u_ctrl_alt[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM l_2u_3h_ctrl_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_2u_3h_ctrl_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM l_2u_3u_shift_alt[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_2u_3u_shift_alt[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM l_1_2h_3h_shift_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_1_2h_3h_shift_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM l_1_2h_3u_alt_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_1_2h_3u_alt_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM l_z_ctrl_shift_alt[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_z_ctrl_shift_alt[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM l_1_2u_3h_ctrl_shift_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_1_2u_3h_ctrl_shift_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM l_1_2u_3u_ctrl_alt_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_1_2u_3u_ctrl_alt_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM l_1u_2u_3u_shift_alt_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_1u_2u_3u_shift_alt_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM l_z_o_ctrl_shift_alt_win[] = {KC_, KC_, COMBO_END};
+// const uint16_t PROGMEM r_z_o_ctrl_shift_alt_win[] = {KC_, KC_, COMBO_END};
+
+// map combo names to their keys and the key they trigger
+combo_t key_combos[] = {
+    [L_1_2H_CTRL] = COMBO(l_1_2h_ctrl, KC_LCTL),
+    [R_1_2H_CTRL] = COMBO(r_1_2h_ctrl, KC_RCTL),
+    // [L_1_2U_SHIFT] = COMBO(l_1_2u_shift, KC_),
+    // [R_1_2U_SHIFT] = COMBO(r_1_2u_shift, KC_),
+    // [L_1_3H_ALT] = COMBO(l_1_3h_alt, KC_),
+    // [R_1_3H_ALT] = COMBO(r_1_3h_alt, KC_),
+    // [L_1_3U_WIN] = COMBO(l_1_3u_win, KC_),
+    // [R_1_3U_WIN] = COMBO(r_1_3u_win, KC_),
+    // [L_2H_3H_CTRL_SHIFT] = COMBO(l_2h_3h_ctrl_shift, KC_),
+    // [R_2H_3H_CTRL_SHIFT] = COMBO(r_2h_3h_ctrl_shift, KC_),
+    // [L_2H_3U_CTRL_ALT] = COMBO(l_2h_3u_ctrl_alt, KC_),
+    // [R_2H_3U_CTRL_ALT] = COMBO(r_2h_3u_ctrl_alt, KC_),
+    // [L_2U_3H_CTRL_WIN] = COMBO(l_2u_3h_ctrl_win, KC_),
+    // [R_2U_3H_CTRL_WIN] = COMBO(r_2u_3h_ctrl_win, KC_),
+    // [L_2U_3U_SHIFT_ALT] = COMBO(l_2u_3u_shift_alt, KC_),
+    // [R_2U_3U_SHIFT_ALT] = COMBO(r_2u_3u_shift_alt, KC_),
+    // [L_1_2H_3H_SHIFT_WIN] = COMBO(l_1_2h_3h_shift_win, KC_),
+    // [R_1_2H_3H_SHIFT_WIN] = COMBO(r_1_2h_3h_shift_win, KC_),
+    // [L_1_2H_3U_ALT_WIN] = COMBO(l_1_2h_3u_alt_win, KC_),
+    // [R_1_2H_3U_ALT_WIN] = COMBO(r_1_2h_3u_alt_win, KC_),
+    // [L_Z_CTRL_SHIFT_ALT] = COMBO(l_z_ctrl_shift_alt, KC_),
+    // [R_Z_CTRL_SHIFT_ALT] = COMBO(r_z_ctrl_shift_alt, KC_),
+    // [L_1_2U_3H_CTRL_SHIFT_WIN] = COMBO(l_1_2u_3h_ctrl_shift_win, KC_),
+    // [R_1_2U_3H_CTRL_SHIFT_WIN] = COMBO(r_1_2u_3h_ctrl_shift_win, KC_),
+    // [L_1_2U_3U_CTRL_ALT_WIN] = COMBO(l_1_2u_3u_ctrl_alt_win, KC_),
+    // [R_1_2U_3U_CTRL_ALT_WIN] = COMBO(r_1_2u_3u_ctrl_alt_win, KC_),
+    // [L_1U_2U_3U_SHIFT_ALT_WIN] = COMBO(l_1u_2u_3u_shift_alt_win, KC_),
+    // [R_1U_2U_3U_SHIFT_ALT_WIN] = COMBO(r_1u_2u_3u_shift_alt_win, KC_),
+    // [L_Z_O_CTRL_SHIFT_ALT_WIN] = COMBO(l_z_o_ctrl_shift_alt_win, KC_),
+    // [R_Z_O_CTRL_SHIFT_ALT_WIN] = COMBO(r_z_o_ctrl_shift_alt_win, KC_),
+};
+
--- keyboards/gmmk/tkl/keymaps/awesome/keymap.c
+++ keyboards/gmmk/tkl/keymaps/awesome/keymap.c
@@ -1,43 +1,9 @@
 #include QMK_KEYBOARD_H
-
-enum {
-    ESC_CAPS,
-};
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
-};
+#include "combos.c"
 
 #define CTRL_ESC LCTL_T(KC_ESC) // Define caps lock as MT(CTRL,ESC)
-#define MEH_SPC MEH_T(KC_SPC)
-
-enum combos {
-    COMBO_LCTL,
-    COMBO_LGUI,
-    COMBO_LALT,
-    COMBO_LCTLGUI,
-    // more here...
-
-    COMBO_LENGTH // nifty trick to avoid manually specifying how many combos you have
-};
-
-uint16_t COMBO_LEN = COMBO_LENGTH; // nifty trick continued
-
-// define keys that make up combos
-const uint16_t PROGMEM fd_combo[] = {KC_F, KC_D, COMBO_END};
-const uint16_t PROGMEM fs_combo[] = {KC_F, KC_S, COMBO_END};
-const uint16_t PROGMEM fa_combo[] = {KC_F, KC_A, COMBO_END};
-const uint16_t PROGMEM fds_combo[] = {KC_F, KC_D, KC_S, COMBO_END};
-// more here...
-
-// map combo names to their keys and the key they trigger
-combo_t key_combos[] = {
-    [COMBO_LCTL] = COMBO(fd_combo, KC_LCTL),
-    [COMBO_LGUI] = COMBO(fs_combo, KC_LGUI),
-    [COMBO_LALT] = COMBO(fa_combo, KC_LALT),
-    [COMBO_LCTLGUI] = COMBO(fds_combo, LCTL(KC_LGUI)),
-    // more here...
-};
+#define MEH_SPC MEH_T(KC_SPC) // Space or MEH on hold
+#define L3_TAB LT(L3, KC_TAB) // TAB or Layer L3 on hold
 
 #define BB 0
 #define BJ 1
@@ -45,37 +11,37 @@ combo_t key_combos[] = {
 #define L3 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [BB] = KEYMAP(KC_ESC,          KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_F11,   KC_F12,
-                  KC_GRV,          KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,  KC_EQL,
-                  KC_TAB,          KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_LBRC,  KC_RBRC,
-                  KC_CAPS,         KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,  KC_QUOT,  KC_BSLS,
-                  KC_LSFT,         KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSFT,  KC_ENT,
-                  KC_LCTL,         KC_LGUI,  KC_LALT,  KC_SPC,   KC_RALT,  MO(FN),   KC_APP,   KC_RCTL,  KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT,  KC_BSPC,
-                  KC_NO,           KC_PSCR,  KC_SLCK,  DF(BJ),   KC_INS,   KC_HOME,  KC_PGUP,  KC_DEL,   KC_END,   KC_PGDN),
+    [BB] = KEYMAP(KC_ESC,     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_F11,   KC_F12,
+                  KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,  KC_EQL,
+                  KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_LBRC,  KC_RBRC,
+                  KC_CAPS,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,  KC_QUOT,  KC_BSLS,
+                  KC_LSFT,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSFT,  KC_ENT,
+                  KC_LCTL,    KC_LGUI,  KC_LALT,  KC_SPC,   KC_RALT,  MO(FN),   KC_APP,   KC_RCTL,  KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT,  KC_BSPC,
+                  KC_NO,      KC_PSCR,  KC_SLCK,  DF(BJ),   KC_INS,   KC_HOME,  KC_PGUP,  KC_DEL,   KC_END,   KC_PGDN),
 
-     [BJ] = KEYMAP(TD(ESC_CAPS),    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_F11,   KC_F12,
-                   KC_GRV,          KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,  KC_EQL,
-                   LT(L3, KC_TAB),  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_LBRC,  KC_RBRC,
-                   CTRL_ESC,        KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,  KC_QUOT,  KC_BSLS,
-                   KC_LSPO,         KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSPC,  KC_ENT,
-                   CW_TOGG,         KC_LGUI,  KC_LALT,  MEH_SPC,  KC_RALT,  MO(FN),   KC_APP,   KC_CAPS,  KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT,  KC_BSPC,
-                   KC_NUBS,         KC_PSCR,  KC_SLCK,  DF(BB),   KC_INS,   KC_HOME,  KC_PGUP,  KC_DEL,   KC_END,   KC_PGDN),
+     [BJ] = KEYMAP(KC_ESC,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_F11,   KC_F12,
+                   KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,  KC_EQL,
+                   L3_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_LBRC,  KC_RBRC,
+                   CTRL_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,  KC_QUOT,  KC_BSLS,
+                   KC_LSPO,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSPC,  KC_ENT,
+                   KC_CAPS,   KC_LGUI,  KC_LALT,  MEH_SPC,  KC_RALT,  MO(FN),   KC_APP,   KC_CAPS,  KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT,  KC_BSPC,
+                   KC_NUBS,   KC_PSCR,  KC_SLCK,  DF(BB),   KC_INS,   KC_HOME,  KC_PGUP,  KC_DEL,   KC_END,   KC_PGDN),
 
-    [FN] = KEYMAP(RESET,           KC_MYCM,  KC_WSCH,  KC_CALC,  KC_MSEL,  KC_MPRV,  KC_MNXT,  KC_MPLY,  KC_MSTP,  KC_MUTE,   KC_VOLD,   KC_VOLU,  KC_TRNS,
-                  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
-                  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
-                  KC_TRNS,         KC_TRNS,  RGB_TOG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
-                  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
-                  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_HUD,  RGB_VAD,   RGB_VAI,   RGB_HUI,  KC_TRNS,
-                  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_TOG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_MOD,  RGB_RMOD),
+    [FN] = KEYMAP(RESET,      KC_MYCM,  KC_WSCH,  KC_CALC,  KC_MSEL,  KC_MPRV,  KC_MNXT,  KC_MPLY,  KC_MSTP,  KC_MUTE,   KC_VOLD,   KC_VOLU,  KC_TRNS,
+                  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
+                  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
+                  KC_TRNS,    KC_TRNS,  RGB_TOG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
+                  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
+                  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_HUD,  RGB_VAD,   RGB_VAI,   RGB_HUI,  KC_TRNS,
+                  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_TOG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_MOD,  RGB_RMOD),
 
-    [L3] = KEYMAP(KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
-                  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
-                  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,    KC_TRNS,   KC_TRNS,  KC_TRNS,
-                  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  KC_TRNS,   KC_TRNS,  KC_TRNS,
-                  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
-                  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
-                  KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS)
+    [L3] = KEYMAP(KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
+                  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
+                  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,    KC_TRNS,   KC_TRNS,  KC_TRNS,
+                  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  KC_TRNS,   KC_TRNS,  KC_TRNS,
+                  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
+                  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
+                  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS)
 };
 
 void matrix_init_user(void) {
@@ -87,3 +53,9 @@ void matrix_scan_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
+
+/* Combos should only run on BJ & L3 */
+bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode, keyrecord_t *record) {
+    return layer_state_is(BJ);
+}
+

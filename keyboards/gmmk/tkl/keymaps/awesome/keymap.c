7cbf321950f724ba34b0450dc7d864d26ad3e227
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-01-30 13:26:25 -0600

    changes meh space to backslash

--- keyboards/gmmk/tkl/keymaps/awesome/keymap.c
+++ keyboards/gmmk/tkl/keymaps/awesome/keymap.c
@@ -2,7 +2,7 @@
 #include "combos.c"
 
 #define CTRL_ESC LCTL_T(KC_ESC) // Define caps lock as MT(CTRL,ESC)
-#define MEH_SPC MEH_T(KC_SPC) // Space or MEH on hold
+#define MEH_BSLS MEH_T(KC_BSLS) // Backslash or MEH on hold
 #define L3_TAB LT(L3, KC_TAB) // TAB or Layer L3 on hold
 
 #define BB 0
@@ -22,9 +22,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [BJ] = KEYMAP(KC_ESC,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_F11,   KC_F12,
                    KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,  KC_EQL,
                    L3_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_LBRC,  KC_RBRC,
-                   CTRL_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,  KC_QUOT,  KC_BSLS,
+                   CTRL_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,  KC_QUOT,  MEH_BSLS,
                    KC_LSPO,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSPC,  KC_ENT,
-                   KC_CAPS,   KC_LGUI,  KC_LALT,  MEH_SPC,  KC_RALT,  MO(FN),   KC_APP,   KC_CAPS,  KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT,  KC_BSPC,
+                   KC_CAPS,   KC_LGUI,  KC_LALT,  KC_SPC,   KC_RALT,  MO(FN),   KC_APP,   KC_CAPS,  KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT,  KC_BSPC,
                    KC_NUBS,   KC_PSCR,  KC_SLCK,  DF(BB),   KC_INS,   KC_HOME,  KC_PGUP,  KC_DEL,   KC_END,   KC_PGDN),
 
     [FN] = KEYMAP(RESET,      KC_MYCM,  KC_WSCH,  KC_CALC,  KC_MSEL,  KC_MPRV,  KC_MNXT,  KC_MPLY,  KC_MSTP,  KC_MUTE,   KC_VOLD,   KC_VOLU,  KC_TRNS,

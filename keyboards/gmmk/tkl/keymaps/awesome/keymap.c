5ed0665da2c6015742adf35f7567ba2551a0c5de
Javier Portillo <17260001+javierportillo@users.noreply.github.com>
2024-02-02 14:21:08 -0600

    disables space cadet shifts

--- keyboards/gmmk/tkl/keymaps/awesome/keymap.c
+++ keyboards/gmmk/tkl/keymaps/awesome/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,  KC_EQL,
                    L3_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_LBRC,  KC_RBRC,
                    CTRL_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,  KC_QUOT,  MEH_BSLS,
-                   KC_LSPO,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSPC,  KC_ENT,
+                   KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSFT,  KC_ENT,
                    KC_CAPS,   KC_LGUI,  KC_LALT,  KC_SPC,   KC_RALT,  MO(FN),   KC_APP,   KC_CAPS,  KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT,  KC_BSPC,
                    KC_NUBS,   KC_PSCR,  KC_SLCK,  DF(BB),   KC_INS,   KC_HOME,  KC_PGUP,  KC_DEL,   KC_END,   KC_PGDN),
 
